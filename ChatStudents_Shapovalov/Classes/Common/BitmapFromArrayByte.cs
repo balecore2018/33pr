f1225d95070070bf5be91aae3d89fc4c8a896f76
balecore <balecored@gmail.com>
2026-08-20 13:02:00 +0500

    Разработанные общие классы. Шаповалов

---
+++ ChatStudents_Shapovalov/Classes/Common/BitmapFromArrayByte.cs
@@ -0,0 +1,27 @@
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace ChatStudents_Shapovalov.Classes.Common
+{
+    public class BitmapFromArrayByte
+    {
+        /// <summary>Создание битмапы изображения из массивов байт</summary>
+        public static BitmapImage LoadImage(byte[] imageData)
+        {
+            BitmapImage Image = new BitmapImage();
+            using (var Stream = new MemoryStream(imageData))
+            {
+                Stream.Position = 0;
+                Image.BeginInit();
+                Image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                Image.CacheOption = BitmapCacheOption.OnLoad;
+                Image.UriSource = null;
+                Image.StreamSource = Stream;
+                Image.EndInit();
+            }
+
+            Image.Freeze();
+            return Image;
+        }
+    }
+}
---
+++ ChatStudents_Shapovalov/Classes/Common/Config.cs
@@ -0,0 +1,11 @@
+namespace ChatStudents_Shapovalov.Classes.Common
+{
+    public class Config
+    {
+        public static readonly string config = "Server=student.permaviat.ru;" +
+            "Trusted_Connection=False;" +
+            "Database=***;" +
+            "User=***;" +
+            "Pwd=***;";
+    }
+}
