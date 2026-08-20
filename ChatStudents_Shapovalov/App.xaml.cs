29eeb4a10e7ce13233ec983cf1302b6f6887e8bb
balecore <balecored@gmail.com>
2026-08-20 12:43:00 +0500

    Разработанные модели. Шаповалов

---
+++ ChatStudents_Shapovalov/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ChatStudents_Shapovalov
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
+
---
+++ ChatStudents_Shapovalov/Models/Users.cs
@@ -0,0 +1,35 @@
+namespace ChatStudents_Shapovalov.Models
+{
+    public class Users
+    {
+        /// <summary>Код</summary>
+        public int Id { get; set; }
+
+        /// <summary>Фамилия</summary>
+        public string Lastname { get; set; }
+
+        /// <summary>Имя пользователя</summary>
+        public string Firstname { get; set; }
+
+        /// <summary>Отчество</summary>
+        public string Surname { get; set; }
+
+        /// <summary>Фотография</summary>
+        public byte[] Photo { get; set; }
+
+        /// <summary>Конструктор для заполнения объекта</summary>
+        public Users(string Lastname, string Firstname, string Surname, byte[] Photo)
+        {
+            this.Lastname = Lastname;
+            this.Firstname = Firstname;
+            this.Surname = Surname;
+            this.Photo = Photo;
+        }
+
+        /// <summary>Получить ФИО пользователя</summary>
+        public string ToFIO()
+        {
+            return $"{Lastname} {Firstname} {Surname}";
+        }
+    }
+}
