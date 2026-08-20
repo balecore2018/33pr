59eec60e9a60063f81c419c3a6e7b3a46e7ea7ad
balecore <balecored@gmail.com>
2026-08-20 15:23:00 +0500

    Разработанная главная страница. Шаповалов

---
+++ ChatStudents_Shapovalov/Pages/Main.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace ChatStudents_Shapovalov.Pages
+{
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
